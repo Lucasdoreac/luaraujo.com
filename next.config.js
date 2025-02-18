f3b7f81a7ce6b13fa93ffe65e8f6ce405fe11d82
Lucas Dórea Cardoso <160977944+Lucasdoreac@users.noreply.github.com>
2025-02-18 11:47:17 -0300

    fix: simplify next config

--- next.config.js
+++ next.config.js
@@ -3,17 +3,10 @@ const nextConfig = {
   output: 'export',
   basePath: '/luaraujo.com',
   assetPrefix: '/luaraujo.com/',
-  trailingSlash: true,
   images: {
     unoptimized: true,
   },
-  // Adiciona rewrite para o HTML gerado
-  async rewrites() {
-    return [
-      {
-        source: '/:path*',
-        destination: `/:path*`,
-      },
-    ]
-  },
+  reactStrictMode: true,
 }
+
+module.exports = nextConfig
