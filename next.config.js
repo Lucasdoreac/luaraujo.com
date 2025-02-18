562549b32d287ece6e51f531f75e4fe177ae6f4c
Lucas Dórea Cardoso <160977944+Lucasdoreac@users.noreply.github.com>
2025-02-18 12:01:26 -0300

    fix: update next config for proper asset prefixing

--- next.config.js
+++ next.config.js
@@ -3,9 +3,12 @@ const nextConfig = {
   output: 'export',
   basePath: '/luaraujo.com',
   images: {
-    unoptimized: true
+    unoptimized: true,
   },
-  reactStrictMode: true
+  reactStrictMode: true,
+  trailingSlash: true,
+  // Ensure assets are prefixed correctly
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/luaraujo.com' : '',
 }
 
 module.exports = nextConfig
